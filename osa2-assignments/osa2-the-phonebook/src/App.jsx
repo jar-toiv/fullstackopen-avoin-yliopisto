b99757ea017e1e06b08419cac233dd6db3488392
Jarmo Toivanen <toivanen@protonmail.com>
2023-09-06 13:27:44 +0300

    osa2 the phonebook exercise 2.6

--- osa2-assignments/osa2-the-phonebook/src/App.jsx
+++ osa2-assignments/osa2-the-phonebook/src/App.jsx
@@ -1,23 +1,44 @@
 import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([
+    { contactID: 1, name: 'Kake Randelin' },
+  ]);
   const [newName, setNewName] = useState('');
+
+  const addContact = (e) => {
+    e.preventDefault();
+
+    const newContactObject = {
+      contactID: persons.length + 1,
+      name: newName,
+    };
+    setPersons(persons.concat(newContactObject));
+    setNewName('');
+  };
+
+  const handleContactChange = (e) => {
+    setNewName(e.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addContact}>
         <div>
-          <div>debug: {newName}</div>
-          name: <input />
+          Name: <input value={newName} onChange={handleContactChange} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit">Add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      ...
+      <h2>Contacts</h2>
+      <ul>
+        {persons.map((person) => (
+          <li key={person.contactID}>{person.name}</li>
+        ))}
+      </ul>
+      <div></div>
     </div>
   );
 };
