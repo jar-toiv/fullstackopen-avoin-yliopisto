8511a907039cc8788d599fe2f6fa64a0489237d6
Jarmo Toivanen <toivanen@protonmail.com>
2023-09-07 18:11:16 +0300

    osa2 The phonebook exercise 2.8 done

--- osa2-assignments/osa2-the-phonebook/src/App.jsx
+++ osa2-assignments/osa2-the-phonebook/src/App.jsx
@@ -2,16 +2,22 @@ import { useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { contactID: 1, name: 'Kake Randelin' },
+    { key: 1, name: 'Kake Randelin', number: '040-123456', contactID: 1 },
+    { key: 2, name: 'Irwin Goodman', number: '39-44-5323523', contactID: 2 },
+    { key: 3, name: 'Kirka Saatana', number: '12-43-234345', contactID: 3 },
+    { key: 4, name: 'Lemmy Kilmister', number: '39-23-6423122', contactID: 4 },
   ]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   const addContact = (e) => {
     e.preventDefault();
 
     const newContactObject = {
+      key: persons.length + 1,
       contactID: persons.length + 1,
       name: newName,
+      number: newNumber,
     };
 
     for (const person of [...persons]) {
@@ -29,12 +35,16 @@ const App = () => {
     } else {
       setPersons(persons.concat(newContactObject));
     }
-
     setNewName('');
+    setNewNumber('');
   };
 
   const handleContactChange = (e) => {
     setNewName(e.target.value);
+  };
+
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value);
   };
 
   return (
@@ -42,7 +52,12 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addContact}>
         <div>
-          Name: <input value={newName} onChange={handleContactChange} />
+          Name:
+          <input value={newName} onChange={handleContactChange} />
+        </div>
+        <div>
+          Number:
+          <input value={newNumber} onChange={handleNumberChange}></input>
         </div>
         <div>
           <button type="submit">Add</button>
@@ -51,10 +66,11 @@ const App = () => {
       <h2>Contacts</h2>
       <ul>
         {persons.map((person) => (
-          <li key={person.contactID}>{person.name}</li>
+          <li key={person.key}>
+            {person.name} - {person.number}
+          </li>
         ))}
       </ul>
-      <div></div>
     </div>
   );
 };
