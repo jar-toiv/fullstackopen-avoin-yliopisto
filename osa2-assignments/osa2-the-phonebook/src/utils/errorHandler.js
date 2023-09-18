8d2d22e6dc48aed914d48c76158cc7a321b74cb2
Jarmo Toivanen <toivanen@protonmail.com>
2023-09-18 13:42:28 +0300

    debug helpers

--- osa2-assignments/osa2-the-phonebook/src/utils/errorHandler.js
+++ osa2-assignments/osa2-the-phonebook/src/utils/errorHandler.js
@@ -1,5 +1,12 @@
 const handleServiceError = (error) => {
-  console.log('Service Error', error);
+  //* loggers for debug purposes
+  // console.log('Service Error', error);
+  // console.log('Data:', error.response.data);
+  // console.log('Status:', error.response.status);
+  // console.log('Headers:', error.response.headers);
+  // console.log('Message:', error.message);
+  //! !!!! Feedback from backend
+  // console.log('Content', error.response.data.message);
 };
 
 export default handleServiceError;
